e975868dcd8891a28f67770ba7a656178b42967a
patrickreillynoland <patrickreillynoland@gmail.com>
2017-12-03 14:06:47 -0800

    Answers now randomize

--- client/questions.js
+++ client/questions.js
@@ -1,51 +1,73 @@
 function getQuestions() {
     var question = document.getElementById('Question1');
-    var answer1 = document.getElementById('Answer1');
-    var answer2 = document.getElementById('Answer2');
-    var answer3 = document.getElementById('Answer3');
-    var answer4 = document.getElementById('Answer4');
+    var answertext1 = document.getElementById('Answer1');
+    var answertext2 = document.getElementById('Answer2');
+    var answertext3 = document.getElementById('Answer3');
+    var answertext4 = document.getElementById('Answer4');
 
     question.innerHTML = "";
-    answer1.innerHTML = "";
-    answer2.innerHTML = "";
-    answer3.innerHTML = "";
-    answer4.innerHTML = "";
+    answertext1.innerHTML = "";
+    answertext2.innerHTML = "";
+    answertext3.innerHTML = "";
+    answertext4.innerHTML = "";
 
     axios.get('https://opentdb.com/api.php?amount=1&type=multiple')
         .then(function(res) {
+            var answerGroup = [];
+            let correctA = getCorrectAnswer(res);
+            let wrong1 = getWrongAnswer1(res);
+            let wrong2 = getWrongAnswer2(res);
+            let wrong3 = getWrongAnswer3(res);
+            answerGroup.push(correctA, wrong1, wrong2, wrong3);
+            answerGroup = shuffle(answerGroup);
+
             question.innerHTML = outputQuestion(res);
-            answer1.innerHTML = outputCorrectAnswer(res);
-            answer2.innerHTML = outputWrongAnswer1(res);
-            answer3.innerHTML = outputWrongAnswer2(res);
-            answer4.innerHTML = outputWrongAnswer3(res);
+            answertext1.innerHTML = answerGroup[0].replace(/\"/g, "");
+            answertext2.innerHTML = answerGroup[1].replace(/\"/g, "");
+            answertext3.innerHTML = answerGroup[2].replace(/\"/g, "");
+            answertext4.innerHTML = answerGroup[3].replace(/\"/g, "");
         })
         .catch(function(err) {
             question.innerHTML = '<h4>Sorry, try again</h4>';
             console.log(err);
         })
 }
-
-function outputQuestion(res) {
-    return '<h4>' + JSON.stringify(res.data.results[0].question) + '</h4>';
+// GETs
+function getQuestion(res) {
+    return JSON.stringify(res.data.results[0].question[0]);
 }
 
-function outputCorrectAnswer(res) {
-    return ' <div class="answer" id="correctAnswer">' + JSON.stringify(res.data.results[0].correct_answer).replace(/\"/g, "") + '</div>';
+function getCorrectAnswer(res) {
+    return ' <div class="answer" id="correctAnswer">' + JSON.stringify(res.data.results[0].correct_answer) + '</div>';;
 }
 
-function outputWrongAnswer1(res) {
-    return ' ' + JSON.stringify(res.data.results[0].incorrect_answers[0]).replace(/\"/g, "");
+function getWrongAnswer1(res) {
+    return ' '+ JSON.stringify(res.data.results[0].incorrect_answers[0]);
 }
 
-function outputWrongAnswer2(res) {
-    return ' ' + JSON.stringify(res.data.results[0].incorrect_answers[1]).replace(/\"/g, "");
+function getWrongAnswer2(res) {
+    return ' ' + JSON.stringify(res.data.results[0].incorrect_answers[1])
 }
 
-function outputWrongAnswer3(res) {
-    return ' ' + JSON.stringify(res.data.results[0].incorrect_answers[2]).replace(/\"/g, "");
+function getWrongAnswer3(res) {
+    return ' '+ JSON.stringify(res.data.results[0].incorrect_answers[2]);
 }
 
 function showAnswer() {
     var rightAnswer = document.getElementById('correctAnswer');
     rightAnswer.className += " correct";
+    console.log(getQuestion(), getCorrectAnswer(), getWrongAnswer1(), getWrongAnswer2(), getWrongAnswer3());
 }
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
+
